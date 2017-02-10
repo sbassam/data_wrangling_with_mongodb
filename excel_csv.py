17a2733234bc25e28f83f331bc7a8b1d47138d6b
Soroush Bassam <sbassam@crp.org>
2017-02-10 17:29:49 -0500

    02102017

--- excel_csv.py
+++ excel_csv.py
@@ -24,24 +24,31 @@ def open_zip(datafile):
 def parse_file(datafile):
     workbook = xlrd.open_workbook(datafile)
     sheet = workbook.sheet_by_index(0)
-    data = []
-    print sheet.nrows
-    print sheet.ncols
-    print sheet.cell_type (2, 4)
-    for i in range(sheet.nrows):
-        row = []
-        for j in range(sheet.ncols):
-            if sheet.cell_type(i, j) == 2:
-                xl_date = sheet.cell_value(i,j)
-                date = xlrd.xldate_as_tuple(xl_date, 0)
-                row.append(date)
-            else:
-                row.append(sheet.cell_value(i,j))
-        data.append(row)
+    data = {}
+    # process all rows that contain station data
+    for n in range (1, 9):
+        station = sheet.cell_value(0, n)
+        cv = sheet.col_values(n, start_rowx=1, end_rowx=None)
+
+        maxval = max(cv)
+        maxpos = cv.index(maxval) + 1
+        maxtime = sheet.cell_value(maxpos, 0)
+        realtime = xlrd.xldate_as_tuple(maxtime, 0)
+        data[station] = {"maxval": maxval,
+                         "maxtime": realtime}
+
     print data
-    # YOUR CODE HERE
-    # Remember that you can use xlrd.xldate_as_tuple(sometime, 0) to convert
-    # Excel date to Python tuple of (year, month, day, hour, minute, second)
     return data
 
-parse_file(datafile)
+def save_file(data, filename):
+    with open(filename, "w") as f:
+        w = csv.writer(f, delimiter='|')
+        w.writerow(["Station", "Year", "Month", "Day", "Hour", "Max Load"])
+        for s in data:
+            year, month, day, hour, _ , _= data[s]["maxtime"]
+            w.writerow([s, year, month, day, hour, data[s]["maxval"]])
+
+
+data = parse_file(datafile)
+filename = "2013_Max_Loads.csv"
+save_file(data, filename)
